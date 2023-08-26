3b200735930179439ae0844029528b1181f744d2
DohktorS <somtosherill66@gmail.com>
2023-08-26 16:01:07 -0700

    Config management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-#Using Puppet, create a file in /tmp.
-
-file {'/tmp/holberton':
-content => 'I love Puppet',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
+# Create a file called school in the /tmp directory
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
