1fbfcb67c22e3b8ad9070b15fee1790795ad5f18
DohktorS <somtosherill66@gmail.com>
2023-09-15 08:58:00 -0700

    fix: lint file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,27 +1,2 @@
-# 0-strace_is_your_friend.pp
-class apache_fix {
-  # Ensure Apache package is installed
-  package { 'apache2':
-    ensure => 'installed',
-  }
-
-  # Ensure Apache service is running
-  service { 'apache2':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['apache2'],
-  }
-
-  # You can use the 'file' resource to manage configuration files, if needed.
-  # For example, to manage a custom Apache config file:
-  # file { '/etc/apache2/sites-available/custom.conf':
-  #   ensure => file,
-  #   source => 'puppet:///modules/apache_fix/custom.conf',
-  #   require => Package['apache2'],
-  #   notify => Service['apache2'],
-  # }
-}
-
-# Apply the Puppet manifest
-include apache_fix
-
+# Fixes a php file typo
+exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
